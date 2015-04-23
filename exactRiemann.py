777ba8deb4880909649024b0cf39c3d734791b9a
skific <skific@riteh.hr>
2015-04-23 15:30:44 +0200

    a bit better...

--- exactRiemann.py
+++ exactRiemann.py
@@ -1,8 +1,8 @@
 import math
-import sys
 import numpy as np
 import scipy
 import scipy.optimize
+
 
 def f(p4, p1, p5, rho1, rho5, gamma):
     z = (p4 / p5 - 1.)
@@ -19,37 +19,172 @@ def f(p4, p1, p5, rho1, rho5, gamma):
     return p1 * fact - p4
 
 
-def solve_p4(p1, p5, rho1, rho5, gamma):
-    # ..solve for post-shock pressure by secant method
-    # ..initial guesses
-    p40 = p1
-    p41 = p5
-    f0 = f(p40, p1, p5, rho1, rho5, gamma)
+def calculate_regions(pl, ul, rhol, pr, ur, rhor, gamma=1.4):
+    """
+    Compute regions
+    :param pl:
+    :param ul:
+    :param rhol:
+    :param pr:
+    :param ur:
+    :param rhor:
+    :param gamma:
+    :return:
+    """
+    # ..begin solution
+    # if pl > pr
+    rho1 = rhol
+    p1 = pl
+    u1 = ul
+    rho5 = rhor
+    p5 = pr
+    u5 = ur
 
-    # ..maximum number of iterations and maximum allowable relative error
-    eps = 1.e-5
+    # unless...
+    if pl < pr:
+        rho1 = rhor
+        p1 = pr
+        u1 = ur
+        rho5 = rhol
+        p5 = pl
+        u5 = ul
 
-    err = 1.
-    it = 0
-    itermax = 20
-    while err > eps:
-        f1 = f(p41, p1, p5, rho1, rho5, gamma)
-        if math.fabs((f1-f0)/f1) < eps:
-            break
-        p4 = p41 - (p41 - p40) * f1 / (f1 - f0)
-        error = math.fabs(p4 - p41) / p41
-        if error < eps:
-            break
-        p40 = p41
-        p41 = p4
-        f0 = f1
-        it += 1
-        if it > itermax:
-            print("no convergence!")
-            sys.exit()
-    return p4
+    # ..solve for post-shock pressure
+    p4 = scipy.optimize.fsolve(f, p1, (p1, p5, rho1, rho5, gamma))[0]
 
-def solver():
+    # c..compute post-shock density and velocity
+    z = (p4 / p5 - 1.)
+    c5 = math.sqrt(gamma * p5 / rho5)
+
+    gm1 = gamma - 1.
+    gp1 = gamma + 1.
+    gmfac1 = 0.5 * gm1 / gamma
+    gmfac2 = 0.5 * gp1 / gamma
+
+    fact = math.sqrt(1. + gmfac2 * z)
+
+    u4 = c5 * z / (gamma * fact)
+    rho4 = rho5 * (1. + gmfac2 * z) / (1. + gmfac1 * z)
+
+    # c..shock speed
+    w = c5 * fact
+
+    # c..compute values at foot of rarefaction
+    p3 = p4
+    u3 = u4
+    rho3 = rho1 * (p3 / p1)**(1. / gamma)
+    return p1, rho1, u1, p3, rho3, u3, p4, rho4, u4, p5, rho5, u5, w
+
+
+def calc_positions(pl, pr, p1, p3, rho1, rho3, u3, w, xi, t, gamma):
+    """
+    :return: Head of Rarefaction: xhd,  Foot of Rarefaction: xft,
+            Contact Discontinuity: xcd, Shock: xsh}
+    """
+    c1 = math.sqrt(gamma * p1 / rho1)
+    c3 = math.sqrt(gamma * p3 / rho3)
+    if pl > pr:
+        xsh = xi + w * t
+        xcd = xi + u3 * t
+        xft = xi + (u3 - c3) * t
+        xhd = xi - c1 * t
+    else:
+        # pr > pl
+        xsh = xi - w * t
+        xcd = xi - u3 * t
+        xft = xi - (u3 - c3) * t
+        xhd = xi + c1 * t
+
+    return xhd, xft, xcd, xsh
+
+
+def region_states(pl, pr, p1, p3, p4, p5, rho1, rho3, rho4, rho5, u1, u3, u4, u5):
+    if pl > pr:
+        return {'Region 1': (p1, rho1, u1),
+                'Region 2': 'RAREFACTION',
+                'Region 3': (p3, rho3, u3),
+                'Region 4': (p4, rho4, u4),
+                'Region 5': (p5, rho5, u5)}
+    else:
+        return {'Region 1': (p5, rho5, u5),
+                'Region 2': (p4, rho4, u4),
+                'Region 3': (p3, rho3, u3),
+                'Region 4': 'RAREFACTION',
+                'Region 5': (p1, rho1, u1)}
+
+
+def create_arrays(pl, pr, xl, xr, positions, state1, state3, state4, state5, npts, gamma, t, xi):
+    (xhd, xft, xcd, xsh) = positions
+    (p1, rho1, u1) = state1
+    (p3, rho3, u3) = state3
+    (p4, rho4, u4) = state4
+    (p5, rho5, u5) = state5
+    gm1 = gamma - 1.
+    gp1 = gamma + 1.
+
+    dx = (xr-xl)/(npts-1)
+    x_arr = np.arange(xl, xr, dx)
+    rho = np.zeros(npts, dtype=float)
+    p = np.zeros(npts, dtype=float)
+    u = np.zeros(npts, dtype=float)
+    c1 = math.sqrt(gamma * p1 / rho1)
+    if pl > pr:
+        for i, x in enumerate(x_arr):
+            if x < xhd:
+                rho[i] = rho1
+                p[i] = p1
+                u[i] = u1
+            elif x < xft:
+                u[i] = 2. / gp1 * (c1 + (x - xi) / t)
+                fact = 1. - 0.5 * gm1 * u[i] / c1
+                rho[i] = rho1 * fact ** (2. / gm1)
+                p[i] = p1 * fact ** (2. * gamma / gm1)
+            elif x < xcd:
+                rho[i] = rho3
+                p[i] = p3
+                u[i] = u3
+            elif x < xsh:
+                rho[i] = rho4
+                p[i] = p4
+                u[i] = u4
+            else:
+                rho[i] = rho5
+                p[i] = p5
+                u[i] = u5
+    else:
+        for i, x in enumerate(x_arr):
+            if x < xsh:
+                rho[i] = rho5
+                p[i] = p5
+                u[i] = -u1
+            elif x < xcd:
+                rho[i] = rho4
+                p[i] = p4
+                u[i] = -u4
+            elif x < xft:
+                rho[i] = rho3
+                p[i] = p3
+                u[i] = -u3
+            elif x < xhd:
+                u[i] = -2. / gp1 * (c1 + (xi - x) / t)
+                fact = 1. + 0.5 * gm1 * u[i] / c1
+                rho[i] = rho1 * fact ** (2. / gm1)
+                p[i] = p1 * fact ** (2. * gamma / gm1)
+            else:
+                rho[i] = rho1
+                p[i] = p1
+                u[i] = -u1
+
+    return x_arr, p, rho, u
+
+
+def solver(left_state, right_state, geometry, t, gamma = 1.4, npts = 500):
+
+    (pl, rhol, ul) = left_state
+    (pr, rhor, ur) = right_state
+    (xl, xr, xi) = geometry
+
+    """
     # ..define initial conditions
     # ..state at left of discontinuity
     rhol = 1.0
@@ -76,182 +211,28 @@ def solver():
     # ..spatial interval over which to compute solution
     xl = 0.0
     xr = 1.0
-
+    """
     # ..begin solution
-    if pl > pr:
-        rho1 = rhol
-        p1 = pl
-        u1 = ul
-        rho5 = rhor
-        p5 = pr
-        u5 = ur
-    else:
-        rho1 = rhor
-        p1 = pr
-        u1 = ur
-        rho5 = rhol
-        p5 = pl
-        u5 = ul
+    p1, rho1, u1, p3, rho3, u3, p4, rho4, u4, p5, rho5, u5, w = \
+        calculate_regions(pl, ul, rhol, pr, ur, rhor, gamma)
 
-    p4 = solve_p4(p1, p5, rho1, rho5, gamma)
-    p4_new = scipy.optimize.fsolve(f, p1, (p1, p5, rho1, rho5, gamma))[0]
-    print('p4 = {0} vs p4_new = {1}'.format(p4, p4_new))
-    p4 = p4_new
-    """
-    # ..solve for post-shock pressure by secant method
-    # ..initial guesses
-    p40 = p1
-    p41 = p5
-    f0 = f(p40, p1, p5, rho1, rho5, gamma)
+    positions = calc_positions(pl, pr, p1, p3,
+                               rho1, rho3,
+                               u3, w, xi, t, gamma)
+    print(positions)
+    regions = region_states(pl, pr, p1, p3, p4, p5,
+                            rho1, rho3, rho4, rho5,
+                            u1, u3, u4, u5)
 
-    # ..maximum number of iterations and maximum allowable relative error
-    eps = 1.e-5
+    for region, values in sorted(regions.items()):
+        print('{0:10} ==> {1}'.format(region, values))
 
-    err = 1.
-    it = 0
-    itermax = 20
-    while err > eps:
-        f1 = f(p41, p1, p5, rho1, rho5, gamma)
-        if math.fabs((f1-f0)/f1) < eps:
-            break
-        p4 = p41 - (p41 - p40) * f1 / (f1 - f0)
-        error = math.fabs(p4 - p41) / p41
-        if error < eps:
-            break
-        p40 = p41
-        p41 = p4
-        f0 = f1
-        it += 1
-        if it > itermax:
-            print("no convergence!")
-            sys.exit()
-    """
-    # c..compute post-shock density and velocity
-    z = (p4 / p5 - 1.)
-    c5 = math.sqrt(gamma * p5 / rho5)
-
-    gm1 = gamma - 1.
-    gp1 = gamma + 1.
-    gmfac1 = 0.5 * gm1 / gamma
-    gmfac2 = 0.5 * gp1 / gamma
-
-    fact = math.sqrt(1. + gmfac2 * z)
-
-    u4 = c5 * z / (gamma * fact)
-    rho4 = rho5 * (1. + gmfac2 * z) / (1. + gmfac1 * z)
-
-    # c..shock speed
-    w = c5 * fact
-
-    # c..compute values at foot of rarefaction
-    p3 = p4
-    u3 = u4
-    rho3 = rho1 * (p3 / p1)**(1. / gamma)
-
-    # c..compute positions of waves
-    if pl > pr:
-        c1 = math.sqrt(gamma * p1 / rho1)
-        c3 = math.sqrt(gamma * p3 / rho3)
-
-        xsh = xi + w * t
-        xcd = xi + u3 * t
-        xft = xi + (u3 - c3) * t
-        xhd = xi - c1 * t
-
-        print('{0}{1}{2}{3}'.format('Region'.center(10), 'Density'.center(10),
-                                    'Pressure'.center(10), 'Velocity'.center(10)))
-        print('{0}{1}{2}{3}'.format('1'.center(10), repr(rho1).center(10), repr(p1).center(10), repr(u1).center(10)))
-        print('{0}'.format('RAREFACTION'.center(20)))
-        print('{0}{1:2.4f}\t{2:.4f}\t{3:.4f}'.format('3'.center(10), rho3, p3, u3))
-        print('{0}{1:2.4f}\t{2:.4f}\t{3:.4f}'.format('4'.center(10), rho4, p4, u4))
-        print('{0}{1:2.4f}\t{2:.4f}\t{3:.4f}'.format('5'.center(10), rho5, p5, u5))
-        print('Head of Rarefaction: x = {0}'.format(xhd))
-        print('Foot of Rarefaction: x = {0}'.format(xft))
-        print('Contact Discontinuity : x = {0}'.format(xcd))
-        print('Shock : x = {0}'.format(xsh))
-
-
-        dx = (xr-xl)/(npts-1)
-        x_arr = np.arange(xl, xr, dx)
-        rho_arr = np.zeros(npts, dtype=float)
-        p_arr = np.zeros(npts, dtype=float)
-        u_arr = np.zeros(npts, dtype=float)
-
-        for x, rho, p, u in zip(x_arr, rho_arr, p_arr, u_arr):
-            if x < xhd:
-                rho = rho1
-                p = p1
-                u = u1
-            elif x < xft:
-                u = 2. / gp1 * (c1 + (x - xi) / t)
-                fact = 1. - 0.5 * gm1 * u / c1
-                rho = rho1 * fact ** (2. / gm1)
-                p = p1 * fact ** (2. * gamma / gm1)
-            elif x < xcd:
-                rho = rho3
-                p = p3
-                u = u3
-            elif x < xsh:
-                rho = rho4
-                p = p4
-                u = u4
-            else:
-                rho = rho5
-                p = p5
-                u = u5
-
-    if pr > pl:
-        c1 = math.sqrt(gamma * p1 / rho1)
-        c3 = math.sqrt(gamma * p3 / rho3)
-
-        xsh = xi - w * t
-        xcd = xi - u3 * t
-        xft = xi - (u3 - c3) * t
-        xhd = xi + c1 * t
-
-        # c..and do say what we found
-        print('{0}{1}{2}{3}'.format('Region'.center(10), 'Density'.center(10),
-                                    'Pressure'.center(10), 'Velocity'.center(10)))
-        print('{0}{1:2.4f}\t{2:.4f}\t{3:.4f}'.format('1'.center(10), rho5, p5, u5))
-        print('{0}{1:2.4f}\t{2:.4f}\t{3:.4f}'.format('2'.center(10), rho4, p4, u4))
-        print('{0}{1:2.4f}\t{2:.4f}\t{3:.4f}'.format('3'.center(10), rho3, p3, u3))
-        print('{0}'.format('RAREFACTION'.center(20)))
-        print('{0}{1:2.4f}\t{2:.4f}\t{3:.4f}'.format('5'.center(10), rho1, p1, u1))
-
-        print('Shock : x = {0}'.format(xsh))
-        print('Contact Discontinuity : x = {0}'.format(xcd))
-        print('Foot of Rarefaction: x = {0}'.format(xft))
-        print('Head of Rarefaction: x = {0}'.format(xhd))
-
-        dx = (xr - xl) / (npts - 1)
-        x_arr = np.arange(xl, xr, dx)
-        rho_arr = np.zeros(npts, dtype=float)
-        p_arr = np.zeros(npts, dtype=float)
-        u_arr = np.zeros(npts, dtype=float)
-
-        for x, rho, p, u in zip(x_arr, rho_arr, p_arr, u_arr):
-            if x < xsh:
-                rho = rho5
-                p = p5
-                u = -u1
-            elif x < xcd:
-                rho = rho4
-                p = p4
-                u = -u4
-            elif x < xft:
-                rho = rho3
-                p = p3
-                u = -u3
-            elif x < xhd:
-                u = -2. / gp1 * (c1 + (xi - x) / t)
-                fact = 1. + 0.5 * gm1 * u / c1
-                rho = rho1 * fact ** (2. / gm1)
-                p = p1 * fact ** (2. * gamma / gm1)
-            else:
-                rho = rho1
-                p = p1
-                u = -u1
+    x, p, rho, u = create_arrays(pl, pr, xl, xr, positions,
+                                 (p1, rho1, u1), (p3, rho3, u3),
+                                 (p4, rho4, u4), (p5, rho5, u5),npts, gamma, t, xi)
+    #print(rho)
 
 if __name__ == '__main__':
-    solver()
+    solver(left_state=(1, 1, 0), right_state=(0.1, 0.125, 0.),
+           geometry=(0, 1, 0.5), t=0.2)
 
