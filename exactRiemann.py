0be75b7e8eeeb54b7d181fe05113681a7227de10
skific <skific@riteh.hr>
2015-04-24 11:12:07 +0200

    output

--- exactRiemann.py
+++ exactRiemann.py
@@ -2,27 +2,32 @@ import sod
 import matplotlib.pyplot as plt
 
 if __name__ == '__main__':
-    """
-    usage example
-    """
-    gamma = 1.4
-    positions, regions, values = sod.solve(left_state=(1, 1, 0), right_state=(0.1, 0.125, 0.),
-                                           geometry=(0., 1., 0.5), t=0.2, gamma=gamma)
-    # Let's print positions
-    print('******')
-    for desc, vals in positions.items():
-        print('{0:10} ==> {1}'.format(desc, vals))
-    print('******')
-    # Let's print regions
-    for region, vals in sorted(regions.items()):
-        print('{0:10} ==> {1}'.format(region, vals))
 
+    gamma = 1.4
+    npts = 500
+
+    # note that gamma and npts are default parameters in solve
+    positions, regions, values = sod.solve(left_state=(1, 1, 0), right_state=(0.1, 0.125, 0.),
+                                           geometry=(0., 1., 0.5), t=0.2, gamma=gamma, npts=npts)
+    # Let's print positions
+    print('Positions:')
+    for desc, vals in positions.items():
+        print('{0:10} : {1}'.format(desc, vals))
+
+    # Let's print regions
+    print('Regions:')
+    for region, vals in sorted(regions.items()):
+        print('{0:10} : {1}'.format(region, vals))
+
+    # Finally, let's plot solutions
     p = values['p']
     rho = values['rho']
     u = values['u']
 
+    # Energy and temperature
     E = p/(gamma-1.) + 0.5*u**2
     T = p/rho
+
     plt.figure(1)
     plt.plot(values['x'], p, linewidth=1.5, color='b')
     plt.ylabel('pressure')
