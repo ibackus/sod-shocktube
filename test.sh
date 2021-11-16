b6fbf71673e9875cc957f16dc074ca84a6ad9e0b
Isaac Backus <ibackus@gmail.com>
2021-11-15 17:07:05 -0800

    Add sodtest.py to run some pythonic tests without any testing framework

--- test.sh
+++ test.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 KEEP_FIGURES_OPEN=false MPLBACKEND=tkagg python2 exactRiemann.py
 if [[ "$?" -eq "0" ]]; then
-  echo "Test passed successfully"
+  echo "Example script passed successfully"
 else
-  echo "Test failed"
+  echo "Example script failed"
+  exit 1
+fi
+
+
+python -c "import sodtest; sodtest.run_test()"
+if [[ "$?" -eq "0" ]]; then
+  echo "Tests passed"
+else
+  echo "Tests failed"
   exit 1
 fi
